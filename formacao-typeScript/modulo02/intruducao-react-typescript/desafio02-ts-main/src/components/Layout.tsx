4187500b46073c0836795958efc281fce27bfaf9
Carloseduardomelo <127193594+Carloseduardomelo@users.noreply.github.com>
2023-10-01 18:05:32 -0300

    dell

--- formacao-typeScript/modulo02/intruducao-react-typescript/desafio02-ts-main/src/components/Layout.tsx
+++
@@ -1,9 +0,0 @@
-import { Footer } from "./Footer"
-import { Header } from "./Header/Header"
-import Inputtela from "./input/Input"
-
-export const Layout = ({ children }: any) => {
-  return(
-    <Inputtela />
-  )
-}
