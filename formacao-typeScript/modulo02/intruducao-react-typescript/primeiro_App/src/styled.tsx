51db62fa550418dbdd87473495ce3c78d2980c09
Carloseduardomelo <127193594+Carloseduardomelo@users.noreply.github.com>
2023-10-01 18:50:29 -0300

    .

--- formacao-typeScript/modulo02/intruducao-react-typescript/primeiro_App/src/styled.tsx
+++ formacao-typeScript/modulo02/intruducao-react-typescript/primeiro_App/src/styled.tsx
@@ -1,30 +1,4 @@
-import { createGlobalStyle , styled } from "styled-components"
-
-export const StyeleApp1 = styled.div`
-    width: 100vw;
-    height: 100vh;
-    overflow: hidden;
-`
-
-export const StyeleHearder = styled.div`
-    width: 100%;
-    height: 10%;
-`
-
-export const StyleApp = styled.div`
-    width: 100%;
-    height: 100%;
-
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    flex-direction: column;
-    gap: 20px;
-
-`
-
-
-
+import { createGlobalStyle } from "styled-components"
 
 export const GlobalStyle = createGlobalStyle`
     *{
--- formacao-typeScript/modulo02/intruducao-react-typescript/primeiro_App/test/setupTests.js
+++
@@ -1,5 +0,0 @@
-import '@testing-library/jest-dom';
-
-// Configure Jest
-jest.config.modulePathIgnorePatterns = ['<rootDir>/node_modules/'];
-jest.config.testEnvironment = 'jsdom';
